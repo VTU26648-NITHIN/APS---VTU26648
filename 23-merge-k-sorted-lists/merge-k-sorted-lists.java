b4d602991b5feed1dc51d82ccc723f31e7d66591
VTU26648-NITHIN <vtu26648@veltech.edu.in>
2026-03-09 11:25:23 +0530

    Time: 1 ms (99.88%) | Memory: 46.9 MB (61.06%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,41 +1,27 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
+public class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode dummy = new ListNode(0);
-        ListNode temp = dummy;
-        for(ListNode head:lists){
-            while(head != null){
-                temp.next = new ListNode(head.val);
-                temp = temp.next;
-                head = head.next;
-            }
-        }
-        dummy.next = sortList(dummy.next);
-        return dummy.next;
+        return mL(lists, 0, lists.length - 1);
     }
-    public static ListNode sortList(ListNode head){
-        ListNode temp = head;
-        ArrayList<Integer> list = new ArrayList<>();
-        while(temp != null){
-            list.add(temp.val);
-            temp = temp.next;
+    
+    private ListNode mL(ListNode[] lists, int l, int r) {
+        if (r < l) return null;
+        if (r == l) return lists[r];
+        
+        int mid = (l + r) / 2;
+        ListNode a = mL(lists, l, mid), b = mL(lists, mid + 1, r);
+        ListNode dmHead = new ListNode(0), cur = dmHead;
+        while (a != null && b != null) {
+            if (a.val < b.val) {
+                cur.next = a;
+                a = a.next;
+            } else {
+                cur.next = b;
+                b = b.next;
+            }
+            cur = cur.next;
         }
-        Collections.sort(list);
-        temp = head;
-        for(int i=0; i<list.size(); i++){
-            temp.val = list.get(i);
-            temp = temp.next;
-        }
-
-        return head;
+        cur.next = (a != null) ? a : b;
+        
+        return dmHead.next;
     }
 }
